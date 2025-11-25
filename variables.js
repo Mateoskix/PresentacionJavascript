ebb75a0cae803126fec7c083fc2170884bc00c36
Mateoskix <mateogt0903@gmail.com>
2025-11-24 20:17:16 -0500

    feat: creacion archivo actividad 2

---
+++ variables.js
@@ -0,0 +1,22 @@
+let promedio = (70 + 82 + 50 + 20) / 4;
+
+console.log(promedio);
+
+let num1 = 6;
+
+let resultadoNum1 = num1 ** 3;
+
+console.log(resultadoNum1)
+
+let nombre = "Mateo"
+
+let edad;
+
+edad = 22;
+
+let ciudad = "Medellín";
+let residencia = `${ciudad} Colombia`;
+
+//console.log(`Me llamo ${nombre}, tengo ${edad} y vivo en ${residencia}`)
+
+console.log("Me llamo"+ " "+nombre+" tengo")
